4a35ac8a42398d314994e6e4191610a725bff725
唐俊杰 <junjie.tang@cmge.com>
2015-01-22 15:32:09 +0800

    添加一个类

---
+++ Prectice/JTDiViewController.h
@@ -0,0 +1,13 @@
+//
+//  JTDiViewController.h
+//  Prectice
+//
+//  Created by 唐俊杰 on 15-1-22.
+//  Copyright (c) 2015年 junjietang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTDiViewController : UIViewController
+
+@end
---
+++ Prectice/JTDiViewController.m
@@ -0,0 +1,50 @@
+//
+//  JTDiViewController.m
+//  Prectice
+//
+//  Created by 唐俊杰 on 15-1-22.
+//  Copyright (c) 2015年 junjietang. All rights reserved.
+//
+
+#import "JTDiViewController.h"
+
+@interface JTDiViewController ()
+
+@end
+
+@implementation JTDiViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor grayColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
